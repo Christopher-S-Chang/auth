beecec8b676fe4f3e3ff3e6c4c3237e574738982
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-02-27 03:00:57 +0000

    session 8 code along

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,32 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
-    # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      # or use if @user, if present?
+      
+      # 2. if the user exists -> check if they know their password
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        
+      # begin user session
+        cookies["name"] = "Cookie Monster"
+        session["user_id"] = @user["id"]
+
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      end# 3. if they know their password -> login is successful
+      # 4. if the user doesn't exist or they don't know their password -> login fails
+      
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/companies"
+    end
   end
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
